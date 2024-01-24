8ce2fcf623426f33b6bd73dbd177021b710328ef
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-01-24 21:21:13 +0300

    Completed javascript side + minor fixes
    Completed javascript side of the project,
    plus made minor fixes to the HTML and CSS

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const operandButtons = document.querySelectorAll('.operandButton');
 const operationButtons = document.querySelectorAll('.operatorButton');
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
+const resultBtn = document.querySelector('#result');
+const clearBtn = document.querySelector('#clear');
+const cleanEntryBtn = document.querySelector('#ce');
+const periodBtn = document.querySelector('.period');
 
 operandButtons.forEach((button) => {
     button.addEventListener(('click'), (event) => {
@@ -15,31 +19,98 @@ operandButtons.forEach((button) => {
 
 function updateInputScreen(number) {
     if (input.textContent.length < 10) {
-        inputOperand += number;
+        inputOperand += number.toString();
     }
     input.textContent = `${inputOperand}`;
 }
 
-function updateOutputScreen(number) {
-    if (output.textContent.length < 10) {
-        outputOperand += number;
-    }
-    output.textContent = `${outputOperand}`;
+function forceUpdateInput(number) {
+    inputOperand = number.toString();
+    input.textContent = number;
+}
+
+function forceUpdateOutput(number) {
+    outputOperand = number.toString();
+    output.textContent = number;
+}
+
+function reset() {
+    inputOperand = '';
+    outputOperand = '';
+    selectedOperator = '';
+    output.textContent = '0';
+    input.textContent = '0';
 }
 
 operationButtons.forEach((button) => {
     button.addEventListener(('click'), (event) => {
+        if (outputOperand === '') {
+            forceUpdateOutput(inputOperand);
+            inputOperand = '';
+            input.textContent = '0';
+        }
         switch (event.target.id) {
             case 'addition':
-                break;
-            case 'substaction':
-                break;
+            case 'substraction':
             case 'multiplication':
-                break;
             case 'division':
-                break;
-            case 'result':
-                break;
+                selectedOperator = event.target.id
         }
     });
 });
+
+periodBtn.addEventListener(('click'), (event) => {
+    if (!(inputOperand.includes('.')) && inputOperand.length > 0) {
+        inputOperand += '.';
+        input.textContent = `${inputOperand}`;        
+    }
+});
+
+resultBtn.addEventListener(('click'), () => {
+    if (outputOperand === '') {
+        reset();
+    } else if (output.textContent.length < 11 && input.textContent.length < 11){
+        switch(selectedOperator) {
+            case 'addition':
+                forceUpdateOutput(addition(inputOperand, outputOperand));
+                break;
+            case 'substraction':
+                forceUpdateOutput(substraction(inputOperand, outputOperand));
+                break;
+            case 'multiplication':
+                forceUpdateOutput(multiplication(inputOperand, outputOperand));
+                break;
+            case 'division':
+                forceUpdateOutput(division(inputOperand, outputOperand));
+                break;
+        }
+    }
+});
+
+cleanEntryBtn.addEventListener(('click'), () => {
+    if (input.textContent.length > 1) {
+        inputOperand = inputOperand.toString().slice(0, -1);
+        input.textContent = inputOperand.toString();
+    } else if (input.textContent.length === 0 || input.textContent.length === 1) {
+        inputOperand = '';
+        input.textContent = '0';
+    }
+});
+
+clearBtn.addEventListener(('click'), reset);
+
+function addition(num1, num2) {
+    return Number(num2) + Number(num1);
+}
+
+function substraction(num1, num2) {
+    return Number(num2) - Number(num1);
+}
+
+function multiplication(num1, num2) {
+    return Math.round((Number(num2) * Number(num1)) * 100) / 100;
+}
+
+function division(num1, num2) {
+    return Math.round((Number(num2) / Number(num1)) * 100) / 100;
+}
